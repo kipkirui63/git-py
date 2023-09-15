08139f02c8cad8d6db524356c05b1278cc8b5c06
kipkirui63 <enocksang8356@gmail.com>
2023-09-15 09:40:07 +0300

    defoned const work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -29,21 +29,21 @@ const Data = [
     },
 ]
 
-// const Work = () => {
-//   return (
-//     <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-//       <h1 className="text-4xl font-bold text-center text-orange-600">Work</h1>
-//       {Data.map((item, idx) => (
-//         <WorkItem
-//           key={idx}
-//           year={item.year}
-//           title={item.title}
-//           duration={item.duration}
-//           details={item.details}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
+const Work = () => {
+  return (
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-orange-600">Work</h1>
+      {Data.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
+    </div>
+  );
+};
 
 // export default Work;
