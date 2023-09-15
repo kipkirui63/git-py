ede8e2ebcb0aba48b7e4123c760fece1688d65d5
kipkirui63 <enocksang8356@gmail.com>
2023-09-15 09:45:04 +0300

    added paragraphs

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -7,15 +7,15 @@ const WorkItem = ({ year, title, duration, details }) => {
         <li className="mb-10 ml-4">
             <div className="absolute w-3 h-3 bg-stone-500 rounded-full mt-1.5 -left-1.5 border-white"/>
             <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-//                 <span className="inline-block px-2 py-1 font-semibold text-white bg-gray-700 rounded-md">{year}</span>
-//                 <span className="text-lg font-semibold text-orange-600">{title}</span>
-//                 <span className="my-1 text-sm font-normal leading-none text-stone-700">{duration}</span>
-//             </p>
-//             <p className="my-2 text-base font-normal text-stone-700">{details}</p>
-//         </li>
-//         </ol >
+                <span className="inline-block px-2 py-1 font-semibold text-white bg-gray-700 rounded-md">{year}</span>
+                <span className="text-lg font-semibold text-orange-600">{title}</span>
+                <span className="my-1 text-sm font-normal leading-none text-stone-700">{duration}</span>
+            </p>
+            <p className="my-2 text-base font-normal text-stone-700">{details}</p>
+        </li>
+        </ol >
         
-//     )
-// }
+    )
+}
 
 // export default WorkItem;
