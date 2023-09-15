c0ab7e7cf5f557efc6e072aa08e401b512604e6c
kipkirui63 <enocksang8356@gmail.com>
2023-09-15 09:21:12 +0300

    created main function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,15 @@ import { TypeAnimation } from "react-type-animation";
 // import {FaTwitter, FaFacebookF,Fainstagram, FaLinkedinIn} from "react-icons/fa"
 // import { GrTwitter } from "react-icons/gr";
 
-// const Main = () => {
-//     return (
-//         <div id="main">
-//             <img className="w-full h-screen object-cover object-left" src="https://tinyurl.com/27g9nh2x"></img>
-//             <div className="w-full h-screen absolute top-0 bg-white/20">
-//                 <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-//                     <h1 className="sm:text-5xl text-4xl font-extrabold text-orange-600">I'm Sang</h1>
-//                     <h2 className="flex sm-text-3x text-2xl font-semibold text-white">
-//                         <TypeAnimation
+const Main = () => {
+    return (
+        <div id="main">
+            <img className="w-full h-screen object-cover object-left" src="https://tinyurl.com/27g9nh2x"></img>
+            <div className="w-full h-screen absolute top-0 bg-white/20">
+                <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+                    <h1 className="sm:text-5xl text-4xl font-extrabold text-orange-600">I'm Sang</h1>
+                    <h2 className="flex sm-text-3x text-2xl font-semibold text-white">
+                        <TypeAnimation
 //       sequence={[
 //         'Software Engineer',
 //         2000,
