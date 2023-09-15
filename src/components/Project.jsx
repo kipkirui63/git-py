72757538c79644b6eb0fafec651ea906d500445f
kipkirui63 <enocksang8356@gmail.com>
2023-09-15 09:25:51 +0300

    defined contant Projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,13 +6,13 @@ import standupImg from "../assets/standup.png"
 import Comp2Img from "../assets/comp2.png"
 
 
-// const Projects = () => {
-//     return (
-//         <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-//             <h1 className="text-4xl font-bold text text-orange-600 text-center">Projects</h1>
-//             <p className="text-center py-8">
-//                 The following are some of my projects in software development. These structured endeavors encompass planning, execution, testing, and deployment, providing hands-on experience that reinforces theoretical concepts and contributes significantly to my growth as a skilled software engineer.
-//                 </p>
+const Projects = () => {
+    return (
+        <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+            <h1 className="text-4xl font-bold text text-orange-600 text-center">Projects</h1>
+            <p className="text-center py-8">
+                The following are some of my projects in software development. These structured endeavors encompass planning, execution, testing, and deployment, providing hands-on experience that reinforces theoretical concepts and contributes significantly to my growth as a skilled software engineer.
+                </p>
 //                 <div className="grid sm:grid-cols-2 gap-12">
 //                     <ProjectItem img={gitlabImg} title="Gitlab" />
 //                     <ProjectItem img={standupImg} title="StandUp"  />
