43cbfbf5463b23170ca857ccf634c690a82b36a9
kipkirui63 <enocksang8356@gmail.com>
2023-09-15 09:30:33 +0300

    added files for onclick

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -8,39 +8,39 @@ const Sidenav = () => {
         setNav(!nav);
     };
     
-//     return (
-//         <div>
-//             <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
-//             {
-//                 nav ? (
+    return (
+        <div>
+            <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
+            {
+                nav ? (
 
-//                     <div className="fixed w-full h-screen bg-grey/90 flex flex-col justify-center items-center z-20">
-//                         <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
-//                             <AiOutlineHome size={20} />
-//                             <span className="pl-4"> Home</span>
-//                         </a>
-//                         <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
-//                             <AiOutlineCheckSquare size={20} />
-//                             <span className="pl-4"> About Me</span>
-//                         </a>
-//                         <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
-//                             <AiOutlineSafetyCertificate size={20} />
-//                             <span className="pl-4">Education</span>
-//                         </a>
-//                         <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
-//                             <AiOutlineProject size={20} />
-//                             <span className="pl-4">Career</span>
-//                         </a>
-//                         <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
-//                             <GrProjects size={20} />
-//                             <span className="pl-4">My Projects</span>
-//                         </a>
-//                         <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
-//                             <AiOutlineMail size={20} />
-//                             <span className="pl-4">Contacts</span>
-//                         </a>
-//                     </div>
-//                 ):(
+                    <div className="fixed w-full h-screen bg-grey/90 flex flex-col justify-center items-center z-20">
+                        <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
+                            <AiOutlineHome size={20} />
+                            <span className="pl-4"> Home</span>
+                        </a>
+                        <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
+                            <AiOutlineCheckSquare size={20} />
+                            <span className="pl-4"> About Me</span>
+                        </a>
+                        <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
+                            <AiOutlineSafetyCertificate size={20} />
+                            <span className="pl-4">Education</span>
+                        </a>
+                        <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
+                            <AiOutlineProject size={20} />
+                            <span className="pl-4">Career</span>
+                        </a>
+                        <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
+                            <GrProjects size={20} />
+                            <span className="pl-4">My Projects</span>
+                        </a>
+                        <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-orange-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease duration-200">
+                            <AiOutlineMail size={20} />
+                            <span className="pl-4">Contacts</span>
+                        </a>
+                    </div>
+                ):(
 //                     ''
 //                 )}
 //                 <div className="md:block hidden fixed top-[25%] z-10">
